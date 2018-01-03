cb8ac8f0ed7f6672a98903e99a908fe312510ccb
ryanlondon <ryel590@gmail.com>
2018-01-03 11:45:01 -0800

    added basic app.js

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="container">Hello photos!</div>
+    );
+  }
+}
+
+export default App;
