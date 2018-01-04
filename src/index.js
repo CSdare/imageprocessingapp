d21d0f8801d1c30b3c8167ee0eac25e9e4b1df47
Erik Larsen <eriklarsen@Eriks-MacBook-Air.local>
2018-01-03 18:40:08 -0800

    add service worker script to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
 import styles from './css/style.css';
-// const si = require('systeminformation');
 
 render(<App />, document.getElementById('root'));
+
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/../workers/serviceWorker.js').then(reg => {
+    if (reg.installing) {
+      console.log('Service worker installing');
+    } else if (reg.waiting) {
+      console.log('Service worker is waiting')
+    } else if (reg.active) {
+      console.log('Service worker is active!')
+    }
+  }).catch(err => {
+    console.log('Registration failed with error: ', err);
+  });
+}
