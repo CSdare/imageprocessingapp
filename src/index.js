7c68a8f70a0ae8bff44e36903875914102c75b0f
Erik Larsen <eriklarsen@Eriks-MacBook-Air.local>
2018-01-03 16:41:25 -0800

    small edits, mostly testing features. no important changes to index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
 import styles from './css/style.css';
+// const si = require('systeminformation');
 
 render(<App />, document.getElementById('root'));
