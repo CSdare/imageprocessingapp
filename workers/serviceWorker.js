0289cc5d55f0e805048c3cbb46038fbd7c4bd69b
Erik Larsen <eriklarsen@Eriks-MacBook-Air.local>
2018-01-05 17:42:02 -0800

    update with code from example website using service worker plugin. need to review

--- workers/serviceWorker.js
+++ workers/serviceWorker.js
@@ -1,6 +1,101 @@
+const DEBUG = true;
+
+const { assets } = global.serviceWorkerOption;
+
+const CACHE_NAME = 'appCache';
+
+let assetsToCache = [...assets, './'];
+
+// get addresses of assets that need to be cached
+assetsToCache = assetsToCache.map(path => {
+  return new URL(path, global.location).toString();
+});
+
+// When the service worker is first downloaded: add core website files during installation
+self.addEventListener('install', event => {
+  if (DEBUG) console.log('SW install event');
+  event.waitUntil(
+    global.caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(assetsToCache)
+      })
+      .then(() => {
+        if (DEBUG) console.log('Cached assets: main', assetsToCache);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  );
+});
+
+// clean the caches - don't really understand how this works
+self.addEventListener('activate', event => {
+  if (DEBUG) console.log('SW Activate event');
+  event.waitUntil(
+    global.caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName.indexOf(CACHE_NAME) === 0) return null;
+          return global.caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+// respond to fetch requests
+self.addEventListener('fetch', event => {
+  const request = event.request;
+  // ignore non-GET request
+  if (request.method !== 'GET') {
+    if (DEBUG) console.log(`SW ignore non-GET request ${request.method}`);
+    return;
+  }
+  const requestUrl = new URL(request.url);
+  // ignore request from different origin
+  if (requestUrl.origin !== location.origin) {
+    if (DEBUG) console.log(`SW Ignore different origin ${requestUrl.origin}`);
+    return;
+  }
+  const resource = global.caches.match(request).then(response => {
+    if (response) {
+      if (DEBUG) console.log(`[SW] fetch URL ${requestUrl.href} from cache`);
+      return response;
+    }
+    //Load and cache known assets
+    return fetch(request)
+      .then(responseNetwork => {
+        if (DEBUG) console.log('responseNetwork is ', responseNetwork);
+        if (!responseNetwork || !responseNetwork.ok) {
+          if (DEBUG) {
+            console.log(`[SW] URL [${requestUrl.toString()}] wrong responseNetwork: ${responseNetwork.status} ${responseNetwork.type}`);
+          }
+          return responseNetwork;
+        }
+        if (DEBUG) console.log(`[SW] URL ${requestUrl.href} fetched`);
+        const responseCache = responseNetwork.clone();
+        global.caches.open(CACHE_NAME)
+          .then(cache => {
+            return cache.put(request, responseCache);
+          })
+          .then(() => {
+            if (DEBUG) console.log(`[SW] Cache asset: ${requestUrl.href}`);
+          });
+        return responseNetwork;
+      })
+      .catch(() => {
+        // User is landing on our page
+        if (event.request.mode === 'navigate') return global.caches.match('/');
+        return null;
+      });
+  });
+  event.respondWith(resource);
+});
+
+
+/*
 // adds files to cache once the service worker has been installed
 // stored in cache with Request object as key and Response object as value
-
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
@@ -42,3 +137,5 @@ self.addEventListener('fetch', function(event) {
     }
   }));
 });
+
+*/
