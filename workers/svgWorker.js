18710fee1650c8056397ce1bd070068b6c77e6bd
ryanlondon <ryel590@gmail.com>
2018-01-10 14:33:51 -0800

    stash changes

--- workers/svgWorker.js
+++ workers/svgWorker.js
@@ -2,10 +2,13 @@ self.Buffer = Buffer;
 
 // const { makeSVG } = require('../functions/makeSVG');
 // import { makeSVG } from '../functions/makeSVG';
+import * as ptjs from 'potrace-js/src';
+
+
 
 
 
 onmessage = (e) => {
-  console.log(self.Buffer);
+  console.log(ptjs);
   // console.log(e.data._id, e.data.url);
 };
