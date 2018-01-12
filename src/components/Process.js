eeb7a1b4918cd7c9e711f2e3cded456b793ad936
ryanlondon <ryel590@gmail.com>
2018-01-12 12:24:55 -0800

    made prop names more specific

--- src/components/Process.js
+++ src/components/Process.js
@@ -3,7 +3,7 @@ const React = require('react');
 const Process = (props) => {
   return (
     <div className="process">
-      <button onClick={props.processImages}>Process Images</button>
+      <button onClick={props.processImages}>Server Process Images</button>
       <button onClick={props.processImagesWorker}>Worker Process Images</button>
       <button onClick={props.processImagesSingle}>Single Thread Process Images</button>
     </div>
