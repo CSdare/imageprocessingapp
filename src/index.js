b4fd8836501f1bb5105f1ac821a7173246382e19
ryanlondon <ryel590@gmail.com>
2018-01-15 13:48:22 -0800

    cleaned it up for demo

--- src/index.js
+++ src/index.js
@@ -49,24 +49,24 @@ if ('serviceWorker' in navigator) {
 
 // this creates 7 web workers and dispatches them to calculate 29th through 49th fib numbers without cache
 
-const createAndDispatchWorkers = () => {
-  for (let i = 1; i <= 7; i++) {
-    let webWorker = new Worker;
-    webWorker.onmessage = e => console.log(e.data);
-    webWorker.onerror = err => console.log('webWorker error:', err);
-    webWorker.postMessage({ arguments: [i + 28] });
-    webWorker.postMessage({ arguments: [i + 35] });
-    webWorker.postMessage({ arguments: [i + 42] });
-  }
-}
+// const createAndDispatchWorkers = () => {
+//   for (let i = 1; i <= 7; i++) {
+//     let webWorker = new Worker;
+//     webWorker.onmessage = e => console.log(e.data);
+//     webWorker.onerror = err => console.log('webWorker error:', err);
+//     webWorker.postMessage({ arguments: [i + 28] });
+//     webWorker.postMessage({ arguments: [i + 35] });
+//     webWorker.postMessage({ arguments: [i + 42] });
+//   }
+// }
 
 // standard fib function without cache
 
-const nthFib = num => {
-  if (num === 0) return 0;
-  if (num === 1) return 1;
-  return nthFib(num - 1) + nthFib(num - 2);
-}
+// const nthFib = num => {
+//   if (num === 0) return 0;
+//   if (num === 1) return 1;
+//   return nthFib(num - 1) + nthFib(num - 2);
+// }
 
 // using worker-loader module to include workers in bundle.js
 // const worker = new Worker;
@@ -78,30 +78,30 @@ const nthFib = num => {
 
 // has main thread calculate fib numbers 29-49 without cache
 
-const singleThreadFibonaccis = () => {
-  for (let i = 29; i <= 49; i++) {
-    console.log('single thread fib #' + i + ': ' + nthFib(i));
-  }
-}
+// const singleThreadFibonaccis = () => {
+//   for (let i = 29; i <= 49; i++) {
+//     console.log('single thread fib #' + i + ': ' + nthFib(i));
+//   }
+// }
 
 // event handler for clicking switch button on DOM - switches whether Fibonacci calculation
 // will be handled by single browser thread or web workers
 
-document.getElementById('switch').addEventListener('click', () => {
-  const switchButton = document.getElementById('switch');
-  if (switchButton.className === 'singleThread') {
-    switchButton.className = 'workerThread';
-    switchButton.innerHTML = 'switch to single thread';
-  } else {
-    switchButton.className = 'singleThread';
-    switchButton.innerHTML = 'switch to worker threads';
-  }
-});
+// document.getElementById('switch').addEventListener('click', () => {
+//   const switchButton = document.getElementById('switch');
+//   if (switchButton.className === 'singleThread') {
+//     switchButton.className = 'workerThread';
+//     switchButton.innerHTML = 'switch to single thread';
+//   } else {
+//     switchButton.className = 'singleThread';
+//     switchButton.innerHTML = 'switch to worker threads';
+//   }
+// });
 
 // event handler for button that will run Fibonacci calculation
 
-document.getElementById('fib').addEventListener('click', () => {
-  const switchButton = document.getElementById('switch');
-  switchButton.className === 'singleThread' ? singleThreadFibonaccis() : createAndDispatchWorkers();
-});
+// document.getElementById('fib').addEventListener('click', () => {
+//   const switchButton = document.getElementById('switch');
+//   switchButton.className === 'singleThread' ? singleThreadFibonaccis() : createAndDispatchWorkers();
+// });
 
